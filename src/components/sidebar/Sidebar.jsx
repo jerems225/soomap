b2abb4401062544a2b0bc4246af692bb3665caea
Jeremy Niondo <jeremieniondo@gmail.com>
2024-07-26 12:34:34 +0000

    customize search zone

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,7 +3,9 @@ import './Sidebar.css';
 const SideBar = () => {
     return (
         <aside className="sidebar">
-
+            <div className="menu_aside">
+                
+            </div>
         </aside>
     )
 }
