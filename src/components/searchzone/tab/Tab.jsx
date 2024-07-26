3da9305d5e2b81a0fe9fd62b8fc6afe9c6bb720e
Jeremy Niondo <jeremieniondo@gmail.com>
2024-07-26 12:25:37 +0000

    customize search zone

--- src/components/searchzone/tab/Tab.jsx
+++ src/components/searchzone/tab/Tab.jsx
@@ -4,28 +4,16 @@ const Tab = () => {
     return (
         <div className="tabs">
             <div className="tab">
-
+                <h4>Ecoles</h4>
             </div>
             <div className="tab">
-
+                <h4>Hotels</h4>
             </div>
             <div className="tab">
-
+                <h4>Pharmacies</h4>
             </div>
             <div className="tab">
-
-            </div>
-            <div className="tab">
-
-            </div>
-            <div className="tab">
-
-            </div>
-            <div className="tab">
-
-            </div>
-            <div className="tab">
-
+                <h4>Eglises</h4>
             </div>
         </div>
     )
